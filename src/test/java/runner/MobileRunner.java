705b24f04de1922f3469147a4ba6a355eaa29100
Tech Global <techglobal.school@gmail.com>
2022-03-08 13:09:12 -0600

    added runner, apps

--- src/test/java/runner/MobileRunner.java
+++ src/test/java/runner/MobileRunner.java
@@ -6,9 +6,9 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/mobile", // path of feature files
-        glue = "mobile/stepDefs",  // path of step defs package
-        tags = "@classTaskScenarioOutlineOnDevice",  // Tags of scenarios to run them
+        features = "src/test/resources", // path of feature files
+        glue = "stepDefinitions",  // path of step defs package
+        tags = "",  // Tags of scenarios to run them
         plugin = {"pretty", "html:target/cucumber-reports/mobile/Cucumber.html"}
         // ,dryRun = true
         // ,monochrome = true // printing the result to console log in more readable way
